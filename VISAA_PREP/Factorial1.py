117402505be38ddeeba333025d9b51afc09eee87
Bhuvaneswari-Singaraju <161604600+Bhuvaneswari-Singaraju@users.noreply.github.com>
2024-11-15 18:40:16 +0530

    Commit Factorial1.py

--- VISAA_PREP/Factorial1.py
+++ VISAA_PREP/Factorial1.py
@@ -1,7 +1,7 @@
 def factorial(num):
-    res = 1
-    for i in range(1, num+1):
-        res *= i
-    return res
+    if num == 0:
+        return 1
+    else:
+        return num * factorial(num-1)
 N = int(input())
 print(factorial(N))
