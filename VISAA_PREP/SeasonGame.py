579ac7ee4bd7fd8d10c431d30f22310d860b4d5f
Bhuvaneswari-Singaraju <161604600+Bhuvaneswari-Singaraju@users.noreply.github.com>
2024-11-16 15:41:21 +0530

    Create SeasonGame.py

---
+++ VISAA_PREP/SeasonGame.py
@@ -0,0 +1,12 @@
+A = int(input())
+if A > 12:
+    print("Invalid")
+else:
+    if 3 <= A <= 5:
+        print("Spring")
+    elif 6<= A <= 8:
+        print("Summer")
+    elif 9<= A <= 11:
+        print("Autumn")
+    else:
+        print("Winter")
